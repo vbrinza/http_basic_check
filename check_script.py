7e4bc14a477bebf29b0ba90da4d8bbc7945bea1c
Vitalie Brinza <231pithagora079@gmail.com>
2018-04-27 22:24:47 +0300

    Update check_script.py

--- check_script.py
+++ check_script.py
@@ -7,15 +7,18 @@ import argparse
 parser=argparse.ArgumentParser()
 parser.add_argument('-u', help='Host to be checked')
 args = parser.parse_args()
+2xx = ['200', '201', '202']
+4xx = ['400', '401', '402']
+5xx = ['500', '501', '502']
 
 r = requests.get(args.u)
-if r.status_code == 200:
+if r.status_code in 2xx:
     print("OK")
     sys.exit(0)
-elif r.status_code == 404:
+elif r.status_code in 4xx:
     print("WARNING")
     sys.exit(1)
-elif r.status_code == 500:
+elif r.status_code in 5xx:
     print("CRITICAL")
     sys.exit(2)
 else:
